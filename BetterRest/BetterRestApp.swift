9481f8164d5d9a6d0669edb9fd29135121c18cb9
Kevin Becker <kevin@kevinbecker.org>
2021-06-26 20:36:52 -0400

    Initial Commit

---
+++ BetterRest/BetterRestApp.swift
@@ -0,0 +1,17 @@
+//
+//  BetterRestApp.swift
+//  BetterRest
+//
+//  Created by Kevin Becker on 6/26/21.
+//
+
+import SwiftUI
+
+@main
+struct BetterRestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
